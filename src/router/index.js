64ef3e22788b11c56925e64abad01fcefae589c1
zhousg <zhoushugang@itcast.cn>
2019-09-01 10:07:39 +0800

    评论管理

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import Test from '@/views/test'
 import Article from '@/views/article'
 import Image from '@/views/image'
 import Publish from '@/views/publish'
+import Comment from '@/views/comment'
 
 import store from '@/store'
 
@@ -29,7 +30,8 @@ const router = new VueRouter({
         { path: '/', name: 'welcome', component: Welcome },
         { path: '/article', name: 'article', component: Article },
         { path: '/image', name: 'image', component: Image },
-        { path: '/publish', name: 'publish', component: Publish }
+        { path: '/publish', name: 'publish', component: Publish },
+        { path: '/comment', name: 'comment', component: Comment }
       ]
     },
     { path: '/test', name: 'test', component: Test },
